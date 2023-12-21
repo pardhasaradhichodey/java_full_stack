1a38fdc301a2fbf9d350df02dc833ad7114d2cd1
pardhasaradhichodey <pardhu.dhoni@gmail.com>
2023-12-20 20:44:10 -0800

    Day 2

---
+++ basics/Datatypes.java
@@ -0,0 +1,22 @@
+class Datatypes
+{
+    public static void main(String []args)
+    {
+        int num1 = 3;
+        byte by = 5;
+        short sh = 500;
+        long l = 5467l;
+        float f = 6.5f;
+        double d= 5.7;
+        char c = 'A';
+        boolean b = true;
+        System.out.println(num1);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+    }
+};
---
+++ basics/Variables.java
@@ -0,0 +1,10 @@
+class Variables
+{
+    public static void main(String []args)
+    {
+        int num1 = 3;
+        int num2 = 5;
+        System.out.println(num1+num2);
+        System.out.println(8-4);
+    }
+};
